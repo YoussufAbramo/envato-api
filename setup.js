69222ee6dd90702b3ce353cbfa688fcdd1747f5d
Youssuf Abramo <84648094+YoussufAbramo@users.noreply.github.com>
2023-12-12 16:42:20 +0400

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,11 @@
+// SETUP Your API Here...
+// Edit envatoAPI_data with your API data so the entire code works correctly
+const envatoAPI_data = {
+  redirect_uri: 'https://abramo.xyz/envato',
+  tokenUrl: 'https://api.envato.com/token',
+  clientId: 'abramoxyz-4hwhyoxs',
+  clientSecret: 'yEkML5SeWpkQ2w0aZYOXWJLLtPJGKcii',
+  authorizationCode: authorizationCode,
+}
+
+export { envatoAPI_data };
